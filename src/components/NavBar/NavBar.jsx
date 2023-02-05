466c1af4612fb06213119fcbc74b9c07b590f714
Bryan Highhill <bryanhighhill@gmail.com>
2023-02-05 09:33:25 -0600

    added material UI to the project. created material ui buttons in the navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,9 @@
 import { useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import './NavBar.css';
 
 const NavBar = () => {
     const history = useHistory();
@@ -23,10 +26,12 @@ const NavBar = () => {
         <>
             <h2>STATUS BAR</h2>
             <div className="status-bar">
-                <button disabled={navDisabled} className="feeling-button" onClick={() => history.push('/')}>Feeling (1/4)</button>
-                <button disabled={navDisabled} className="understanding-button" onClick={() => history.push('/understanding')}>Understanding (2/4)</button>
-                <button disabled={navDisabled} className="support-button" onClick={() => history.push('/support')}>Support (3/4)</button>
-                <button disabled={navDisabled} className="comments-button" onClick={() => history.push('/comments')}>Comments (4/4)</button>
+                <Stack className="material-ui-buttons" direction="row" spacing={5}>
+                    <Button disabled={navDisabled} className={navDisabled ? "feeling-nav" : "feeling-nav-complete"} onClick={() => history.push('/')}>Feeling (1/4)</Button>
+                    <Button disabled={navDisabled} className="understanding-button" onClick={() => history.push('/understanding')}>Understanding (2/4)</Button>
+                    <Button disabled={navDisabled} className="support-button" onClick={() => history.push('/support')}>Support (3/4)</Button>
+                    <Button disabled={navDisabled} className="comments-button" onClick={() => history.push('/comments')}>Comments (4/4)</Button>
+                </Stack>
             </div>
         </>
     )
