a54dbbd5c3b72ac938ab4b2b7ca9a3a4b39c505e
Bryan Highhill <bryanhighhill@gmail.com>
2023-02-07 15:23:12 -0600

    created a Reducers.js file to hold all the reducers. Exporting from there, importing into index.js and consuming in the reduxStore

--- src/index.js
+++ src/index.js
@@ -5,73 +5,7 @@ import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { useState } from 'react';
-import axios from 'axios';
-
-const feeling = (state = 0, action) => {
-    if (action.type === 'SET_FEELING') {
-        return action.payload;
-    } 
-    return state;
-}
-
-const understanding = (state = 0, action) => {
-    if (action.type === 'SET_UNDERSTANDING') {
-        return action.payload;
-    } 
-    return state;
-}
-
-const support = (state = 0, action) => {
-    if (action.type === 'SET_SUPPORT') {
-        return action.payload;
-    } 
-    return state;
-}
-
-const comments = (state = '', action) => {
-    if (action.type === 'SET_COMMENTS') {
-        return action.payload;
-    } 
-    return state;
-}
-
-//keep track of if a page has been "submitted"
-const feelingCompleted = (state = false, action) => {
-    if (action.type === 'SET_FEELING_COMPLETED') {
-        return action.payload;
-    }
-    return state;
-}
-
-const understandingCompleted = (state = false, action) => {
-    if (action.type === 'SET_UNDERSTANDING_COMPLETED') {
-        return action.payload;
-    }
-    return state;
-}
-
-const supportCompleted = (state = false, action) => {
-    if (action.type === 'SET_SUPPORT_COMPLETED') {
-        return action.payload;
-    }
-    return state;
-}
-
-const commentsCompleted = (state = false, action) => {
-    if (action.type === 'SET_COMMENTS_COMPLETED') {
-        return action.payload;
-    }
-    return state;
-}
-
-//SET feedbackList with GET data
-const feedbackList = (state = [], action) => {
-    if (action.type === 'SET_FEEDBACK_LIST') {
-        return action.payload;
-    }
-    return state;
-};
+import { feeling, understanding, support, comments, feelingCompleted, understandingCompleted, supportCompleted, commentsCompleted, feedbackList } from '../src/components/Reducers/Reducers';
 
 const reduxStore = createStore(
     combineReducers({
